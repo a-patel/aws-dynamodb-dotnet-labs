77bd7e3695bcfd52e51731563a4b46b739578f05
Ashish Patel <toaashishpatel@gmail.com>
2020-02-24 00:39:44 +0530

    Added properties in Domain enitity and model and AWS DynamoDB SDK

--- src/AwsDynamoDBLabs.Web/Entities/Movie.cs
+++ src/AwsDynamoDBLabs.Web/Entities/Movie.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿#region Imports
+using Amazon.DynamoDBv2.DataModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+#endregion
 
 namespace AwsDynamoDBLabs.Web.Entities
 {
+    [DynamoDBTable("Movie")]
     public class Movie
     {
+        [DynamoDBHashKey]
+        public int Id { get; set; }
+
+        [DynamoDBGlobalSecondaryIndexHashKey]
+        public string Name { get; set; }
+
+        public string Director { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public List<string> Actors { get; set; } = new List<string>();
     }
 }
--- src/AwsDynamoDBLabs.Web/Models/MovieModel.cs
+++ src/AwsDynamoDBLabs.Web/Models/MovieModel.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿#region Imports
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+#endregion
 
 namespace AwsDynamoDBLabs.Web.Models
 {
     public class MovieModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Director { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public List<string> Actors { get; set; } = new List<string>();
     }
 }
